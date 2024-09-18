6e6bafd44ce00076f6efae69b8997a41e63ac56f
CifronPro <ars132465@gmail.com>
2024-09-18 23:58:00 +0700

    user menu add

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,19 +9,83 @@ import static java.lang.Math.sqrt;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(ex5("erttre"));
-        /*Scanner in = new Scanner(System.in);
-        System.out.print("Input Ex: ");
-        int ex = in.nextInt();
+        System.out.println(" ");
+        System.out.println("0: Exit;");
+        System.out.println("1-5: Ex1-Ex5;");
+        System.out.println("6: All;");
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            System.out.println(" ");
+            System.out.print("Input: ");
+            int ex = in.nextInt();
 
-        switch(ex){
-            case 1:
-                List<String> result = ex1(500);
-                System.out.println(result);
-            case 2:
+            switch (ex) {
+                case 0:
+                    return;
+                case 1:
+                    System.out.print("Result: ");
+                    System.out.println(ex1(500));
+                    continue;
+                case 2:
+                    System.out.print("Result: ");
+                    System.out.println(ex2("make install"));
+                    continue;
+                case 3:
+                    System.out.print("Input a: ");
+                    int a = in.nextInt();
+                    System.out.print("Input b: ");
+                    int b = in.nextInt();
+                    System.out.print("Input c: ");
+                    int c = in.nextInt();
+                    System.out.print("Result: ");
+                    if(a == 0){
+                        System.out.println("'a' must be not zero!");
+                        continue;
+                    }
+                    System.out.println(ex3(a, b, c));
+                    continue;
+                case 4:
+                    System.out.print("Result: ");
+                    System.out.println(ex4(2));
+                    continue;
+                case 5:
+                    System.out.print("Input string: ");
+                    String input = in.next();
+                    System.out.print("Result: ");
+                    System.out.println(ex5(input));
+                    continue;
+                case 6:
+                    System.out.println("Ex1: ");
+                    System.out.print("Result: ");
+                    System.out.println(ex1(500));
+                    System.out.println(" ");
 
+                    System.out.println("Ex2: ");
+                    System.out.print("Result: ");
+                    System.out.println(ex2("make install"));
+                    System.out.println(" ");
+
+                    System.out.println("Ex3: ");
+                    System.out.println("Input: a: 3; b: 2; c: 4");
+                    System.out.print("Result: ");
+                    System.out.println(ex3(3,2,4));
+                    System.out.println(" ");
+
+                    System.out.println("Ex4: ");
+                    System.out.print("Result: ");
+                    System.out.println(ex4(2));
+                    System.out.println(" ");
+
+                    System.out.println("Ex5: ");
+                    System.out.println("Input: qwerrewq");
+                    System.out.print("Result: ");
+                    System.out.println(ex5("qwerrewq"));
+                    System.out.println(" ");
+
+                    continue;
+            }
+            in.close();
         }
-        in.close();*/
     }
     public static List<String> ex1(int n){
         List<String> result = new ArrayList<>();
